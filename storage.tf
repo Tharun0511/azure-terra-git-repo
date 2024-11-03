beda058a51f08f3f6c76d66402cdc715ead2add0
GATTU THARUN KUMAR <tharun.kumar1552@gmail.com>
2024-11-03 15:12:16 +0530

    Adding storage account tf code

--- storage.tf
+++ storage.tf
@@ -1,7 +1,3 @@
-data "azurerm_resource_group" "RG-1" {
-  name = "learn-d7810094-c1d2-4248-912d-2e6b54eaec44"
-}
-
 resource "azurerm_storage_account" "example" {
   name                     = "storsimple9898"
   resource_group_name      = data.azurerm_resource_group.RG-1.name
